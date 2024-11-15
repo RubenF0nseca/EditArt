dd123a714e0b507810695fcb2c71f48c18425bff
VulpeOlesea <leseavulpe@gmail.com>
2024-11-15 18:27:26 +0000

    update

--- EditArt/resources/views/admin/dashboard.blade.php
+++ EditArt/resources/views/admin/dashboard.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
-</div>
+@extends('layout.admin.base')
+
+@section('content')
+    <h2>Admin Dashboard</h2>
+@endsection
