ae7e7c1881ec407edb1fe891d3ffd6ad197cfa55
VulpeOlesea <leseavulpe@gmail.com>
2024-11-23 22:39:19 +0000

    add banner + menu guest

---
+++ EditArt/resources/views/layouts/guest/parts/menu.blade.php
@@ -0,0 +1,17 @@
+{{--Navegação--}}
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3" >
+    <div class="container"><a class="navbar-brand" href="#"><img src="/public/icons/favicon-32x32.png" alt="EditArt" width="60" /><span class="text-1000 fs-1 ms-2 fw-medium">Edit<span class="fw-bold">Art</span></span></a>
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
+                <li class="nav-item"><a class="nav-link active active" aria-current="page" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Livros</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Autores </a></li>
+            </ul>
+            <form class="d-flex py-3 py-lg-0">
+                <button class="btn btn-link text-1000 fw-medium order-1 order-lg-0" type="button">Registar</button>
+                <button class="btn btn-enter rounded-pill order-0" type="submit">Entrar</button>
+            </form>
+        </div>
+    </div>
+</nav>
