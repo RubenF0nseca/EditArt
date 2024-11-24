887ed1b9a0a55c27775e8250d2fcc6f8b574ba25
Lucas Patrício <62174653+LucasPatricio3@users.noreply.github.com>
2024-11-24 17:38:28 +0000

    Books added to web.php

--- EditArt/routes/web.php
+++ EditArt/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,4 @@ Route::get('/admin/dashboard', function () {
 });
 
 Route::resource('users', UserController::class);
+Route::resource('books', BookController::class);
