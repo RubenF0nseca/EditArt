4b2197249e2b1216b48e675e30a9a5c5fa6b8d74
VulpeOlesea <leseavulpe@gmail.com>
2024-11-28 13:21:41 +0000

    update dashboard

--- EditArt/resources/views/admin/dashboard.blade.php
+++ EditArt/resources/views/admin/dashboard.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <h2>Admin Dashboard</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 d-flex justify-content-center">
+                <img src="{{asset('imgs/logo-editart.png')}}" height="100%" alt="EditArt">
+            </div>
+        </div>
+    </div>
 @endsection
