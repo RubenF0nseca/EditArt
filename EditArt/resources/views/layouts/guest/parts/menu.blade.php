152c060026ac451fa7accb984da0fd9e6c22cd46
VulpeOlesea <leseavulpe@gmail.com>
2024-12-27 02:11:49 +0000

    update navbar

--- EditArt/resources/views/layouts/guest/parts/menu.blade.php
+++ EditArt/resources/views/layouts/guest/parts/menu.blade.php
@@ -1,4 +1,4 @@
-{{--Navegação--}}
+<!-- Navegação ------------------------------------  -->
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" >
     <div class="container"><a class="navbar-brand" href="{{ route('home') }}"><img src={{asset('imgs/logo-editart.png')}} alt="EditArt" width="50" /></a>
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-menu" aria-controls="sidebar-menu" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -6,11 +6,13 @@
             <ul class="navbar-nav mx-auto border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
                 <li class="nav-item"><a class="nav-link active active" aria-current="page" href="{{ route('home')}}">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('guest.books') }}">Livros</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('guest.authors') }}">Autores </a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('guest.authors') }}">Autores</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Online Editora</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('client.forum') }}">Forum</a></li>
             </ul>
             <form class="d-flex py-3 py-lg-0">
                 <a href="{{ route('registration') }}" class="btn btn-link text-1000 fw-medium order-1 order-lg-0" type="submit">Registar</a>
-                <a href="{{ route('login') }}" class="btn btn-enter rounded-pill order-0" type="submit">Entrar</a>
+                <a href="{{ route('login') }}" class="btn btn-darknew rounded-pill order-0" type="submit">Entrar</a>
             </form>
         </div>
     </div>
