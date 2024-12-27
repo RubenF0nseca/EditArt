96daf5fa4166ee43987ed312daf36d3b5e711e87
VulpeOlesea <leseavulpe@gmail.com>
2024-12-27 00:11:39 +0000

    update footer

--- EditArt/resources/views/layouts/guest/parts/footer.blade.php
+++ EditArt/resources/views/layouts/guest/parts/footer.blade.php
@@ -1,13 +1,48 @@
 {{--Footer--}}
 <footer class="footer footer-transparent d-print-none">
-    <div class="container-xl"></div>
-    <div class="row text-center align-items-center flex-row-reverse">
-        <div class="col">
-            <p>2024 © EditArt</p>
-            <p class="text-center">
-                <a href="https://github.com/RubenF0nseca/EditArt" class="git link-body-emphasis text-decoration-none">
-                    <i class="fa-brands fa-github"></i>
-                </a>
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="widget">
+                    <h5 class="widget-title font-alt">EditArt</h5>
+                    <p>Exemplo Exemplo Exemplo.</p>
+                    <p>Phone: +1 234 567 89 10</p>
+                    <p>Email:<a href="#">Exemplo@example.com</a></p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="widget">
+                    <h5 class="widget-title font-alt">Sobre nós</h5>
+                    <ul class="icon-list">
+                        <li>Exemplo <a href="#">Exemplo</a></li>
+                        <li>Exemplo <a href="#">Exemplo</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="widget">
+                    <h5 class="widget-title font-alt">Subscribe now</h5>
+                    <form id="subscription-form" role="form" method="post" action="php/subscribe.php">
+                        <div class="input-group">
+                            <input class="form-control" type="email" id="semail" name="semail" placeholder="Your Email" data-validation-required-message="Please enter your email address." required="required"/><span class="input-group-btn">
+                        <button class="btn btn-darknew" id="subscription-form-submit" type="submit">Submit</button></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-sm-6 font-alt">
+                <p>2025 © EditArt, All Rights Reserved</p>
+            </div>
+            <div class="col-sm-6">
+                <p class="text-end">
+                    <a href="https://github.com/RubenF0nseca/EditArt" class="git link-body-emphasis text-decoration-none">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </p>
+            </div>
         </div>
     </div>
 </footer>
