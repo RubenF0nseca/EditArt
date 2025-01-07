8942073936fbfc98cbe913fdf2d3e17e45bbe94b
VulpeOlesea <leseavulpe@gmail.com>
2025-01-07 12:24:53 +0000

    add LoginController

---
+++ EditArt/app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLogin(){
+        return view('login.show');
+    }
+
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+
+            $request->session()->regenerate();
+            if (auth()->user()->hasRole('admin'))
+                return redirect()->route('admin.dashboard');
+            else
+                return redirect()->intended(route('client.dashboard'));
+        }
+
+        return redirect()->back()->withErrors([
+            'error' => 'Verifique as suas credênciais!'
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect(route('home'));
+    }
+}
