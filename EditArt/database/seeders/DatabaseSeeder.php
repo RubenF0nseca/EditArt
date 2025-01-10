6d3b838749616c184c3f26010b5ebdaec05e32ff
RubenF0nseca <153688053+RubenF0nseca@users.noreply.github.com>
2025-01-10 18:37:30 +0000

    install sanctum/spatie

--- EditArt/database/seeders/DatabaseSeeder.php
+++ EditArt/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'edit.art.buisness@gmail.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' => \Str::random(10),
+            'address' => fake()->address(),
+            'nif' => fake()->randomNumber(9),
+            'phone_number' => fake()->randomNumber(9),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+
+            'birthdate' => fake()->dateTimeBetween('-30 year', '-10 year'),
+            'role' => fake()->numberBetween(1, 5),
+        ]);
         User::factory(10)->create();
 
         $this->call([
