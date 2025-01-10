2ec504045a52e573d5784fbaef6291f0cd0c8ccc
RubenF0nseca <153688053+RubenF0nseca@users.noreply.github.com>
2025-01-10 18:53:37 +0000

    UserSeeder

---
+++ EditArt/database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'edit.art.buisness@gmail.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' => \Str::random(10),
+            'address' => fake()->address(),
+            'nif' => fake()->randomNumber(9),
+            'phone_number' => fake()->randomNumber(9),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+
+            'birthdate' => fake()->dateTimeBetween('-30 year', '-10 year'),
+            'role' => fake()->numberBetween(1, 5),
+        ]);
+        User::factory(10)->create();
+    }
+}
