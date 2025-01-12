232533c258695834276f7d279430d38d2398dea3
VulpeOlesea <leseavulpe@gmail.com>
2025-01-12 20:09:44 +0000

    update web

--- EditArt/routes/web.php
+++ EditArt/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
