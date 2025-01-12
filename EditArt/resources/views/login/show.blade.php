c56d9dfe70ab796315a5209089ede8d227c84fa4
VulpeOlesea <leseavulpe@gmail.com>
2025-01-12 01:37:55 +0000

    update position + css: log e reg

--- EditArt/resources/views/login/show.blade.php
+++ EditArt/resources/views/login/show.blade.php
@@ -3,48 +3,50 @@
 @section('content')
     <div class="page-wrapper wrapper d-flex flex-column min-vh-100">
         <section class="banner">
-            <div class="row" id="margin-top">
-                <div class="col-md-6 ">
-                    <div class="text-container2">
-                        <h1>“Think before you speak.<br>Read before you think.”</h1>
-                        <h5>― Fran Lebowitz</h5>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-5">
-                    <div class="card shadow-lg border-0 rounded-lg mb-5">
-                        <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
-                        <div class="card-body">
-                            @if($errors->any())
-                                <div class="row p-2">
-                                    <x-alert id="" type="danger">
-                                        Verifique os dados inseridos
-                                        <ul>
-                                            @foreach($errors->all() as $message)
-                                                <li>{{$message}}</li>
-                                            @endforeach
-                                        </ul>
-                                    </x-alert>
-                                </div>
-                            @endif
-
-                            <form method="POST" action="{{route('login')}}">
-                                @csrf
-                                <div class="form-floating mb-3">
-                                    <input value="{{old('email')}}" class="form-control" id="inputEmail" type="email" placeholder="" name="email"/>
-                                    <label for="inputEmail">name@example.com</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input class="form-control" id="inputPassword" type="password" placeholder="" name="password"/>
-                                    <label for="inputPassword">Password</label>
-                                </div>
-                                <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                    <a class="small" href="#">Esqueceu-se da Password?</a>      {{--TODO fix or delete --}}
-                                    <button class="btn btn-primary" type="submit" name="bt_login">Entrar</button>
-                                </div>
-                            </form>
+            <div class="container">
+                <div class="row" id="margin-top">
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <div class="text-container2 mb-5">
+                            <h1>“Think before you speak.<br> Read before you think.”</h1>
+                            <h5 class="text-end">― Fran Lebowitz</h5>
                         </div>
-                        <div class="card-footer text-center py-3">
-                            <div class="small"><a href="{{route('registration')}}">Não tem conta? Registe-se!</a></div>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <div class="card shadow-lg border-0 rounded-lg">
+                            <div class="card-header"><h3 class="text-center font-weight-light my-4 font-alt">Login</h3></div>
+                            <div class="card-body">
+                                @if($errors->any())
+                                    <div class="row p-2">
+                                        <x-alert id="" type="danger">
+                                            Verifique os dados inseridos
+                                            <ul>
+                                                @foreach($errors->all() as $message)
+                                                    <li>{{$message}}</li>
+                                                @endforeach
+                                            </ul>
+                                        </x-alert>
+                                    </div>
+                                @endif
+
+                                <form method="POST" action="{{route('login')}}">
+                                    @csrf
+                                    <div class="form-floating mb-3">
+                                        <input value="{{old('email')}}" class="form-control" id="inputEmail" type="email" placeholder="" name="email"/>
+                                        <label for="inputEmail">name@example.com</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="inputPassword" type="password" placeholder="" name="password"/>
+                                        <label for="inputPassword">Password</label>
+                                    </div>
+                                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                        <a class="small" href="#">Esqueceu-se da Password?</a>      {{--TODO fix or delete --}}
+                                        <button class="btn btn-primary" type="submit" name="bt_login">Entrar</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="card-footer text-center py-3">
+                                <div class="small"><a href="{{route('registration')}}">Não tem conta? Registe-se!</a></div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- EditArt/resources/views/registration/show.blade.php
+++ EditArt/resources/views/registration/show.blade.php
@@ -3,77 +3,79 @@
 @section('content')
     <div class="page-wrapper wrapper d-flex flex-column min-vh-100">
         <section class="banner">
-            <div class="row mt-5">
-                <div class="col-md-6 ">
-                    <div class="text-container2">
-                        <h1>“Think before you speak.<br>Read before you think.”</h1>
-                        <h5>― Fran Lebowitz</h5>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-5 mt-5">
-                    <div class="card shadow-lg border-0 rounded-lg mb-5">
-                        <div class="card-header"><h3 class="text-center font-weight-light my-4">Criar Conta</h3></div>
-                        <div class="card-body">
-                            <form method="post">
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3 mb-md-0">
-                                            <input class="form-control" id="inputName" type="text" placeholder="" name="usr_name"/>
-                                            <label for="inputName">Nome</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input class="form-control" id="inputNif" type="text" placeholder="" name="usr_nif"/>
-                                            <label for="inputNif">NIF</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input class="form-control" id="inputEmail" type="email" placeholder="" name="log_email"/>
-                                    <label for="inputEmail">Email</label>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3 mb-md-0">
-                                            <input class="form-control" id="inputTelefone" type="text" placeholder="" name="usr_telefone"/>
-                                            <label for="inputTelefone">Telefone</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3 mb-md-0">
-                                            <input class="form-control" id="inputAddress" type="text" placeholder="" name="usr_address"/>
-                                            <label for="inputAddress">Morada</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3 mb-md-0">
-                                            <input class="form-control" id="inputPassword" type="password" placeholder="" name="log_password"/>
-                                            <label for="inputPassword">Password</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating mb-3 mb-md-0">
-                                            <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="" name="password_check"/>
-                                            <label for="inputPasswordConfirm">Confirmar Password</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="mt-4 mb-0">
-                                    <div class="d-grid">
-                                        <button class="btn btn-primary btn-block" type="submit" name="bt_registration">Criar Conta</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="card-footer text-center py-3">
-                            <div class="small"><a href="{{route('login')}}">Já tem conta? Faça Login!</a></div>
+            <div class="container">
+                <div class="row mb-5" id="margin-top">
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <div class="text-container2 mb-5">
+                            <h1>“Think before you speak.<br>Read before you think.”</h1>
+                            <h5 class="text-end">― Fran Lebowitz</h5>
                         </div>
                     </div>
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <div class="card shadow-lg border-0 rounded-lg">
+                            <div class="card-header"><h3 class="text-center font-weight-light my-4">Criar Conta</h3></div>
+                            <div class="card-body">
+                                <form method="post">
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="inputName" type="text" placeholder="" name="usr_name"/>
+                                                <label for="inputName">Nome</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input class="form-control" id="inputNif" type="text" placeholder="" name="usr_nif"/>
+                                                <label for="inputNif">NIF</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="inputEmail" type="email" placeholder="" name="log_email"/>
+                                        <label for="inputEmail">Email</label>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="inputTelefone" type="text" placeholder="" name="usr_telefone"/>
+                                                <label for="inputTelefone">Telefone</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="inputAddress" type="text" placeholder="" name="usr_address"/>
+                                                <label for="inputAddress">Morada</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="" name="log_password"/>
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-floating mb-3 mb-md-0">
+                                                <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="" name="password_check"/>
+                                                <label for="inputPasswordConfirm">Confirmar Password</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 mb-0">
+                                        <div class="d-grid">
+                                            <button class="btn btn-primary btn-block" type="submit" name="bt_registration">Criar Conta</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="card-footer text-center py-3">
+                                <div class="small"><a href="{{route('login')}}">Já tem conta? Faça Login!</a></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
+            </div>
         </section>
     </div>
 @endsection
