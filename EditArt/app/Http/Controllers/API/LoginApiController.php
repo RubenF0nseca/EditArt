804e95e04c1a277ecb19cb13e671e68eaef46781
Lucas Patrício <62174653+LucasPatricio3@users.noreply.github.com>
2025-01-18 03:57:16 +0000

    LoginAPI Basics + API

---
+++ EditArt/app/Http/Controllers/API/LoginApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginApiController extends BaseController
+{
+    public function login(Request $request){
+        if(Auth::attempt(["email" => $request->email, "password" => $request->password])){
+            $user = Auth::user();
+            $success['token'] = $user->createToken('EditArtToken')->plainTextToken;
+            $success['name'] = $user->name;
+            return $this->sendResponse($success, 'Login efetuado com sucesso.');
+        }
+        else return $this->sendError("Acesso não autorizado.",null, 401);
+    }
+}
