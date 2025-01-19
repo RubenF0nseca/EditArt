d8fb9cf3baa9bc735ec74aad976d5a98d611528d
VulpeOlesea <leseavulpe@gmail.com>
2025-01-19 23:48:45 +0000

    update errors pages

--- EditArt/resources/views/errors/403.blade.php
+++ EditArt/resources/views/errors/403.blade.php
@@ -1,3 +1,14 @@
-<div>
-    error 403
-</div>
+@extends('layouts.client.base') <!--TODO role-->
+
+@section('content')
+    <div class="page-wrapper wrapper d-flex flex-column min-vh-100">
+        <!-- Banner ------------------------------------  -->
+        <div class="error-403 text-center">
+            <h2 id="title-403">403</h2>
+            <h1 id="title-error-403">ERRO</h1>
+            <p id="text-403">Parece que tropeçaste num capítulo bloqueado.<br>
+                Mas não te preocupes, há muitas outras histórias na nossa biblioteca!</p>
+            <x-button.link link="{{ route('guest.books') }}" color="light-new">Voltar para a biblioteca</x-button.link>
+        </div>
+    </div>
+@endsection
--- EditArt/resources/views/errors/404.blade.php
+++ EditArt/resources/views/errors/404.blade.php
@@ -1,4 +1,14 @@
-<div>
-    404
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-</div>
+@extends('layouts.client.base') <!--TODO role-->
+
+@section('content')
+    <div class="page-wrapper wrapper d-flex flex-column min-vh-100">
+        <!-- Banner ------------------------------------  -->
+        <div class="error-404 text-center">
+            <h2 id="title-404">404</h2>
+            <h1 id="title-error-404">ERRO</h1>
+            <p id="text-404">Oh, não! Parece que esta página perdeu-se nos labirintos dos livros.<br>
+                Experimente procurar outra história!</p>
+            <x-button.link link="{{ route('guest.books') }}" color="light-new">Voltar para a biblioteca</x-button.link>
+        </div>
+    </div>
+@endsection
