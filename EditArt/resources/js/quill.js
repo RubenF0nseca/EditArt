81e73fa0b25f3a8b2489196d975dda315afc7b6d
VulpeOlesea <leseavulpe@gmail.com>
2025-01-19 02:08:10 +0000

    update forum + editor

--- EditArt/resources/js/quill.js
+++ EditArt/resources/js/quill.js
@@ -1,16 +1,17 @@
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
-// editor escolhemos o tema aqui e o que queremos que o editor faça
+// Editor escolhemos o tema aqui e o que queremos que o editor faça
 document.addEventListener('DOMContentLoaded', function () {
     const quill = new Quill('#editor-container', {
         theme: 'snow',
-        placeholder: 'Start typing here...',
+        placeholder: 'O seu texto...',
         modules: {
             toolbar: [
                 [{ 'header': [1, 2, false] }],
                 ['bold', 'italic', 'underline'],
                 [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                ['link', 'blockquote', 'image']
             ]
         }
     });
@@ -20,6 +21,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const hiddenInput = document.querySelector('input[name="content"]');
 
     if (form && hiddenInput) {
+        form.addEventListener('submit', function () {
+            hiddenInput.value = quill.root.innerHTML;
+        });
+    }
+});
+
+// Editor para o fórum
+document.addEventListener('DOMContentLoaded', function () {
+    const showEditor = document.getElementById('show-editor');
+    const editorForm = document.getElementById('editor-form');
+
+    showEditor.addEventListener('click', function () {
+        editorForm.style.display = 'block';
+        showEditor.style.display = 'none';
+    });
+
+    const quill = new Quill('#editor-container-2', {
+        theme: 'snow',
+        placeholder: 'O seu texto...',
+        modules: {
+            toolbar: [
+                [{ 'header': [1, 2, false] }],
+                ['bold', 'italic', 'underline'],
+                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                ['link', 'blockquote', 'image']
+            ]
+        }
+    });
+
+    // Capturar dados para a BD
+    const form = document.querySelector('form');
+    const hiddenInput = document.querySelector('input[name="content"]');
+    const topicInput = document.querySelector('input[name="topic"]');
+
+    if (form && hiddenInput && topicInput) {
         form.addEventListener('submit', function () {
             hiddenInput.value = quill.root.innerHTML;
         });
--- EditArt/resources/views/client/forum.blade.php
+++ EditArt/resources/views/client/forum.blade.php
@@ -7,73 +7,115 @@
             <h2 class="module-title font-alt" id="margin-top">Forum</h2>
         </div>
         <div class="container">
-            <blockquote class="section-subtitle font-serif">
-                <p class="quot">&quot; Think before you speak. Read before you think. &quot;</p>
-                <p class="text-end">― Fran Lebowitz</p><hr class="divider">
-            </blockquote>
-            <div class="row">
+
+            <div class="row mt-5">
                 <!-- Forum ------------------------------------  -->
                 <div class="col-sm-8">
-                    <div class="post">
-                        <div class="author-avator set-bg" data-setbg="./assets/images/rp-1.jpg" style="background-image: url(&quot;./assets/images/rp-1.jpg&quot;);"></div>
-                        <div class="post-header font-alt">
-                            <h2 class="post-title"><a href="#">Our trip to the Alps</a></h2>
-                            <div class="post-date">June 21, 2018</div>
-                            <div class="post-meta">By&nbsp;<a href="#">Mark Stone</a>| 3 Comments | <a href="#">Photography, </a><a href="#">Web Design</a>
+
+                    <!-- Barra de pesquisa -->
+                    <div class="widget">
+                        <form role="form">
+                            <div class="search-box mb-5">
+                                <input class="form-control" type="text" placeholder="Procurar..."/>
+                                <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
                             </div>
-                        </div>
-                        <div class="post-entry">
-                            <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
-                            <a class="more-link" href="#">Read more</a>
-                        </div>
+                        </form>
                     </div>
-                    <div class="post">
-                        <div class="author-avator set-bg" data-setbg="./assets/images/rp-1.jpg" style="background-image: url(&quot;./assets/images/rp-1.jpg&quot;);"></div>
-                        <div class="post-header font-alt">
-                            <h2 class="post-title"><a href="#">Our trip to the Alps</a></h2>
-                            <div class="post-date">June 21, 2018</div>
-                            <div class="post-meta">By&nbsp;<a href="#">Mark Stone</a>| 3 Comments | <a href="#">Photography, </a><a href="#">Web Design</a>
-                            </div>
-                        </div>
-                        <div class="post-entry">
-                            <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
-                            <a class="more-link" href="#">Read more</a>
-                        </div>
-                    </div>
-                    <div class="post">
-                        <div class="author-avator set-bg" data-setbg="./assets/images/rp-1.jpg" style="background-image: url(&quot;./assets/images/rp-1.jpg&quot;);"></div>
-                        <div class="post-header font-alt">
-                            <h2 class="post-title"><a href="#">Our trip to the Alps</a></h2>
-                            <div class="post-date">June 21, 2018</div>
-                            <div class="post-meta">By&nbsp;<a href="#">Mark Stone</a>| 3 Comments | <a href="#">Photography, </a><a href="#">Web Design</a>
-                            </div>
-                        </div>
-                        <div class="post-entry">
-                            <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
-                            <a class="more-link" href="#">Read more</a>
-                        </div>
+                    <!-- Forum -->
+                    <x-table>
+                        <x-table.thead>
+                            <x-table.tr>
+                                <x-table.th class="text-center">Author</x-table.th>
+                                <x-table.th>Tema</x-table.th>
+                                <x-table.th>Categoria</x-table.th>
+                                <x-table.th>Comentarios</x-table.th>
+                                <x-table.th>Data</x-table.th>
+                            </x-table.tr>
+                        </x-table.thead>
+                        <x-table.tbody class="forum">
+                            <x-table.tr>
+                                <td class="text-center"><a href="#"><img src="{{asset('imgs/no_user.png')}}" class="author-avatar" alt="">
+                                        <p>Maria Mendes</p></a></td>
+                                <td><h2 class="review-author font-alt align-items-center"><a href="#">Tema Title lorem ipsum</a></h2></td>
+                                <td><p class="font-serif"><a href="#">Blog</a></p></td>
+                                <td><p><a href="#">3 Comentarios</a></p></td>
+                                <td><p><a href="#">June 21, 2018</a></p></td>
+                            </x-table.tr>
+
+                        </x-table.tbody>
+                    </x-table>
+
+                    <!-- Paginação --> <!-- TODO pagination -->
+                    <div class="text-center">Pagination</div>
+
+                    <!-- Botão que chama o formulário -->
+                    <div class="text-center mt-5 mb-5">
+                        <h5>Não conseguiu encontrar o tópico que procurava? Cria um novo tópico.</h5>
+                        <button class="btn btn-solid" id="show-editor">Criar um novo tópico</button>
                     </div>
 
-                    <!-- TODO pagination -->
+                    <!-- Formulário para envio de tópico -->
+                    <div class="editor" id="editor-form" style="display: none;">
+                        <form action="#" method="POST">
+                            @csrf
 
-                    <div class="pagination font-alt"><a href="#"><i class="fa fa-angle-left"></i></a><a class="active" href="#">1</a><a href="#">2</a><a href="#">3</a><a href="#">4</a><a href="#"><i class="fa fa-angle-right"></i></a></div>
+                            <div class="mb-3">
+                                <input type="text" id="topic" name="topic" placeholder="Título" />
+                            </div>
+                            <div id="editor-container-2"></div>
+                            <input type="hidden" id="content" name="content" />
+                            <div class="mt-3 text-end">
+                                <button type="submit" class="btn btn-solid">Enviar</button>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
-                <!-- Forum Categorias ------------------------------------  -->
-                <div class="col-sm-4 col-md-3 col-md-offset-1 sidebar">
-                    <div class="widget">
-                        <h5 class="widget-title font-alt">Forum Categorias</h5>
-                        <ul class="icon-list">
-                            <li><a href="#">Photography - 7</a></li>
-                            <li><a href="#">Web Design - 3</a></li>
-                            <li><a href="#">Illustration - 12</a></li>
-                            <li><a href="#">Marketing - 1</a></li>
-                            <li><a href="#">Wordpress - 16</a></li>
-                        </ul>
+
+                <!-- Regras do Fórum -------------------TODO TEXTO----------------- -->
+                <div class="col-sm-4">
+                    <div class="text-center">
+                        <h2 class="section-title font-alt">Regras do Fórum</h2>
                     </div>
-                    <!-- Tags ------------------------------------  -->
-                    <div class="widget">
-                        <h5 class="widget-title font-alt">Tag</h5>
-                        <div class="tags font-serif"><a href="#" rel="tag">Blog</a><a href="#" rel="tag">Photo</a><a href="#" rel="tag">Video</a><a href="#" rel="tag">Image</a><a href="#" rel="tag">Minimal</a><a href="#" rel="tag">Post</a><a href="#" rel="tag">Theme</a><a href="#" rel="tag">Ideas</a><a href="#" rel="tag">Tags</a><a href="#" rel="tag">Bootstrap</a><a href="#" rel="tag">Popular</a><a href="#" rel="tag">English</a>
+                    <div class="accordion mb-3" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header font-alt">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <i class="fa-solid fa-angles-right"></i>&nbsp A regra principal:
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <div class="font-serif mt-3">
+                                        <h2 class="quot text-center">"Think before you speak.<br> Read before you think."</h2>
+                                        <p class="text-end">― Fran Lebowitz</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <i class="fa-solid fa-angles-right"></i>&nbsp Accordion Item #2
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    <i class="fa-solid fa-angles-right"></i>&nbsp Accordion Item #3
+                                </button>
+                            </h2>
+                            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
