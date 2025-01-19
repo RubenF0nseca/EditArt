a5d22dfa551b81ea2b7916cd421ccadad2b43a3d
VulpeOlesea <leseavulpe@gmail.com>
2025-01-19 14:15:34 +0000

    update navbar

--- EditArt/resources/views/layouts/client/parts/menu.blade.php
+++ EditArt/resources/views/layouts/client/parts/menu.blade.php
@@ -1,6 +1,8 @@
 <!-- Navegação ------------------------------------  -->
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" >
+    <!-- Logo -->
     <div class="container"><a class="navbar-brand" href="{{ route('home') }}"><img src={{asset('imgs/logo-editart.png')}} alt="EditArt" width="50" /></a>
+        <!-- Menu -->
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-menu" aria-controls="sidebar-menu" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="sidebar-menu">
             <ul class="navbar-nav mx-auto border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
@@ -10,6 +12,7 @@
                 <li class="nav-item"><a class="nav-link" href="#">Online Editora</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('client.forum') }}">Forum</a></li>
             </ul>
+            <!-- Icons -->
             <ul class="d-flex py-3 py-lg-0 flex-end top-icons">
                 @role('admin') <li class="nav-item"><a class="nav-link" href="{{ route('admin.dashboard') }}"><i class="fa-solid fa-dashboard"></i>&nbsp Dashboard</a></li>@endrole
                 <li class="nav-item"><a class="nav-link" href="{{ route('client.profile') }}"><i class="fa-solid fa-user"></i></a></li>
@@ -19,12 +22,9 @@
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
                         <button class="nav-link border-0 bg-transparent" type="submit">
-            <span class="nav-link-icon d-md-none d-lg-inline-block">
-                <i class="ti ti-logout fs-5"></i>
-            </span>
                             <span class="nav-link-title">
-                Sair
-            </span>
+                                <i class="fa-solid fa-right-from-bracket"></i>
+                            </span>
                         </button>
                     </form>
                 </li>
