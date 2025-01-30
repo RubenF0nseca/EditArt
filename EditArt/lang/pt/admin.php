e0cf9559e752d9513ec1367235313f3b5c167111
Larruda1984 <154379274+Larruda1984@users.noreply.github.com>
2025-01-30 20:17:59 +0000

    update language

--- EditArt/lang/pt/admin.php
+++ EditArt/lang/pt/admin.php
@@ -33,24 +33,4 @@ return [
     'dashboard_reviews' => 'Avaliações',
 
 
-    //Guest
-    'maintenance' => 'Página em manutenção',
-    'banner' => [
-        'slide_1' => 'Slide 1',
-        'slide_2' => 'Slide 2',
-        'banner_alt' => 'Banner',
-        'previous' => 'Anterior',
-        'next' => 'Próximo',
-    ],
-    'books' => [
-        'our_publications' => 'As nossas publicações',
-        'our_authors_works' => 'Apresentamos as obras dos nossos autores.',
-        'search_by_title' => 'Pesquisar por título do livro',
-        'categories' => 'Categorias',
-        'all_genres' => 'Todos os gêneros',
-        'image_not_available' => 'Imagem não disponível',
-        'add_to_cart' => 'Adicionar ao carrinho',
-    ],
-
-
 ];
