2c7d1fded32414c7c41f07b259a6e6c8d7cdd2b3
Larruda1984 <154379274+Larruda1984@users.noreply.github.com>
2025-01-30 22:30:30 +0000

    update language

--- EditArt/resources/views/cart/confirmation.blade.php
+++ EditArt/resources/views/cart/confirmation.blade.php
@@ -7,40 +7,40 @@
                 <div id="logo-bg"></div>
                 <!-- Confirmação ------------------------------------  -->
                 <div class="col-lg-8 offset-2">
-                    <h1 class="section-title font-alt">Confirmação do pedido</h1>
-                    <h2 class="section-subtitle font-serif">Obrigado pelo seu pedido!</h2>
+                    <h1 class="section-title font-alt">{{ __('cart.confirmation_title') }}</h1>
+                    <h2 class="section-subtitle font-serif">{{ __('cart.thank_you') }}</h2>
                     <hr class="divider">
                     <div class="order-card mb-5 p-5">
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Nome</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.name') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Morada</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.address') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Email</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.email') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Telemóvel</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.phone') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Conta Paypal</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.paypal_account') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Data do pedido</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.order_date') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Total</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.total') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                         <div class="row review-entry">
-                            <span class="col-3 font-alt"><h3>Lista de compras</h3></span>
+                            <span class="col-3 font-alt"><h3>{{ __('cart.shopping_list') }}</h3></span>
                             <span class="col-9">#</span>
                         </div>
                     </div>
