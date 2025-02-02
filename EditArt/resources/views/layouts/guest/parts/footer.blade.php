d6d07bc9f86cc99f8c1de03f01d34e3f41f0ac53
VulpeOlesea <leseavulpe@gmail.com>
2025-02-02 01:33:58 +0000

    update footer

--- EditArt/resources/views/layouts/guest/parts/footer.blade.php
+++ EditArt/resources/views/layouts/guest/parts/footer.blade.php
@@ -1,34 +1,39 @@
-{{--Footer--}}
+<!-- Footer -->
 <footer class="footer footer-transparent d-print-none">
     <div class="container-xl">
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-md-3">
                 <div class="widget">
                     <h5 class="widget-title font-alt">{{ __('homepage.title') }}</h5>
                     <p>{{ __('homepage.description') }}</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="widget">
+                    <h5 class="widget-title font-alt">Os nossos contatos</h5>
                     <p><i class="fa-solid fa-phone-flip"></i>&nbsp +351 234 567 888</p>
                     <p><i class="fa-solid fa-envelope"></i><a href="edit.art@gmail.com">&nbsp edit.art@gmail.com</a></p>
                 </div>
             </div>
-            <div class="col-sm-4">
+            <div class="col-md-3">
                 <div class="widget">
                     <h5 class="widget-title font-alt">{{ __('homepage.services_title') }}</h5>
                     <ul class="icon-list">
                         <li><a href="{{route('guest.books')}}">{{ __('homepage.online_store') }}</a></li>
-                        <li><a href="#">{{ __('homepage.online_publisher') }}</a></li>
+                        <li><a href="{{route('publisher')}}">{{ __('homepage.online_publisher') }}</a></li>
                         <li><a href="{{route('forum')}}">{{ __('homepage.forum') }}</a></li>
                     </ul>
                 </div>
             </div>
-            <div class="col-sm-4">
+            <div class="col-md-3">
                 <div class="widget">
-                    <h5 class="widget-title font-alt">{{ __('homepage.subscription_title') }}</h5>
-                    <form id="subscription-form" role="form" method="post" action="php/subscribe.php">
-                        <div class="input-group">
-                            <input class="form-control" type="email" id="semail" name="semail" placeholder="{{ __('homepage.subscription_placeholder') }}" data-validation-required-message="Please enter your email address." required="required"/><span class="input-group-btn">
-                        <button class="btn btn-light-new" id="subscription-form-submit" type="submit">{{ __('homepage.subscription_submit') }}</button></span>
-                        </div>
-                    </form>
+                    <h5 class="widget-title font-alt">A Nossa Equipe</h5>
+                    <ul class="icon-list">
+                        <li>Leila Arruda</li>
+                        <li>Lucas Patrício</li>
+                        <li>Olesea Vulpe</li>
+                        <li>Rúben Fonseca</li>
+                    </ul>
                 </div>
             </div>
         </div>
