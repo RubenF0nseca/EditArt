332f8c5fdf03355cb7c1da4a4b2e1590b3fde837
VulpeOlesea <leseavulpe@gmail.com>
2025-02-10 00:23:38 +0000

    update post + is_resolved

--- EditArt/resources/views/post/index.blade.php
+++ EditArt/resources/views/post/index.blade.php
@@ -9,6 +9,12 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <!-- Alerta para mensagem de sucesso -->
+            @if(session('success'))
+                <x-alert id="success-alert" type="success">
+                    {{session('success')}}
+                </x-alert>
+            @endif
             <div class="col-md-10 offset-md-1">
                 <div class="card shadow-lg border-0 rounded-lg mt-4">
                     <div class="table-responsive">
@@ -60,3 +66,23 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const successAlert = document.getElementById('success-alert');//
+            if (successAlert) {
+                setTimeout(function() {
+                    // Adiciona a classe 'fade' e remove a classe 'show' para iniciar a transição de fechamento
+                    successAlert.classList.remove('show');
+                    successAlert.classList.add('fade');
+
+                    // Remove o elemento do DOM depois da transição
+                    setTimeout(function() {
+                        successAlert.remove();
+                    }, 500); // Ajuste o tempo conforme o efeito 'fade'
+                }, 3000); // Fecha o alerta após 3 segundos
+            }
+        });
+    </script>
+@endpush
